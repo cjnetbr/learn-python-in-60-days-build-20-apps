ed599fd3219bc1342dff2dcf4d5262f3bd825030
Claudio <cjnetbr@gmail.com>
2024-07-21 10:32:42 -0300

    Seção-4_Dia-3_Aula-33

--- app1/main.py
+++ app1/main.py
@@ -2,17 +2,18 @@ todos = []
 
 while True:
     user_action = input("Type add, show or exit: ")
-
+    user_action = user_action.lower().strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-             print(todos)
+             for item in todos:
+                 print(item)
         case "exit":
             break
 print("Bye!!")
-    
+
 
 
 
