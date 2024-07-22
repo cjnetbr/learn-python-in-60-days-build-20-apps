d9c1afedd21944b08f8b1785466cd0a24a6c1b6c
Claudio Junior <cjnetbr@hotmail.com>
2024-07-22 18:25:29 +0000

    Seção-5_Day-4_aula-42

--- app1/main.py
+++ app1/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.lower().strip()
     match user_action:
         case "add":
@@ -11,12 +11,16 @@ while True:
              for item in todos:
                  item = item.title()
                  print(item)
+        case "edit":
+            number = int(input("Enter the number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
         case _:
             print("Hey, you entered an unknow command")
 print("Bye!!")
-
 
 
 
